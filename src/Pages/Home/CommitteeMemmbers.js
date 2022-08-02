155467cbf88509d85371c5db0acefd37fe1f1eb3
Arif reza <arif.reza3126@gmail.com>
2022-08-02 13:54:09 +0600

    add blog

--- src/Pages/Home/CommitteeMemmbers.js
+++ src/Pages/Home/CommitteeMemmbers.js
@@ -5,16 +5,16 @@ const CommitteeMemmbers = () => {
     return (
         <div>
             {/* This is the heading section of this page  */}
-            <div class="flex flex-wrap justify-center mx-4 mt-32">
-                <div class="w-full px-4">
-                    <div class="text-center mx-auto mb-[60px] lg:mb-20 max-w-[510px]">
-                        <span class="font-semibold text-lg  mb-2 block text-white">
+            <div className="flex flex-wrap justify-center mx-4 mt-32">
+                <div className="w-full px-4">
+                    <div className="text-center mx-auto mb-[60px] lg:mb-20 max-w-[510px]">
+                        <span className="font-semibold text-lg  mb-2 block text-white">
                             Our Committee
                         </span>
-                        <h2 class=" font-bold text-3xl sm:text-4xl md:text-[40px] text-white mb-4">
+                        <h2 className=" font-bold text-3xl sm:text-4xl md:text-[40px] text-white mb-4">
                             Our Committee Members 2022-23
                         </h2>
-                        <p class="text-base text-white">
+                        <p className="text-base text-white">
                             There are many variations of passages of Lorem Ipsum available
                             but the majority have suffered alteration in some form.
                         </p>
@@ -22,84 +22,84 @@ const CommitteeMemmbers = () => {
                 </div>
             </div>
             {/* Committee members container */}
-            <div class="commiteeMemberscontainer">
-                <div class="box">
-                    <div class="imgBox">
+            <div className="commiteeMemberscontainer">
+                <div className="box">
+                    <div className="imgBox">
                         <img src="https://scontent.fdac7-1.fna.fbcdn.net/v/t39.30808-6/272619609_2148501048652105_5399183547860637310_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHsYjn_zuX7f8svxBdT64r0BS7979sbMwUFLv3v2xszBWkbAgCsplN7SLjxifO8wF_50sfsCWFXET9elXW4k8L-&_nc_ohc=JOfQQaDG6hAAX_48-Uj&_nc_ht=scontent.fdac7-1.fna&oh=00_AT_e5HLnLvyMdpdbnh4lH5dm4gthMoBO9JlnA82VRVBu7g&oe=62EB08C9" alt="" />
                     </div>
-                    <div class="content">
+                    <div className="content">
                         <h2>Md. Aminul Islam<br />
                             <span>ADVISOR</span></h2>
                     </div>
                 </div>
-                <div class="box">
-                    <div class="imgBox">
+                <div className="box">
+                    <div className="imgBox">
                         <img src="https://img.freepik.com/free-photo/portrait-handsome-young-man-makes-okay-gesture-demonstrates-agreement-likes-idea-smiles-happily-wears-optical-glasses-yellow-hat-t-shirt-models-indoor-its-fine-thank-you-hand-sign_273609-30676.jpg?size=626&ext=jpg" alt="" />
                     </div>
-                    <div class="content">
+                    <div className="content">
                         <h2>Imran Nazir<br />
                             <span>TRAINER</span></h2>
                     </div>
                 </div>
-                <div class="box">
-                    <div class="imgBox">
-                        <img src="https://scontent.fdac7-1.fna.fbcdn.net/v/t1.6435-9/143890057_1305412279815054_7819741331719585223_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeFdLQU4IwZMhYYgIecQKKimMfFw4HvmoKMx8XDge-ago1J6Zl3G57WsS16AtJ6LZR6EfEgyQyI9C1eC4_cSYxoH&_nc_ohc=TnDbSafOuI8AX_yjuMO&_nc_ht=scontent.fdac7-1.fna&oh=00_AT-BOci_PRIimk7JIMhdvNi3k4eu44x_laO5UfXUpjJ2WQ&oe=630B2318" alt="" />
+                <div className="box">
+                    <div className="imgBox">
+                        <img src="./images/Banjir-ah.jpeg" alt="" />
                     </div>
-                    <div class="content">
+                    <div className="content">
                         <h2>Banjir Ahmad<br />
                             <span>PRECIDENT</span></h2>
                     </div>
                 </div>
-                <div class="box">
-                    <div class="imgBox">
+                <div className="box">
+                    <div className="imgBox">
                         <img src="https://scontent.fdac7-1.fna.fbcdn.net/v/t39.30808-6/271995115_3120809278178725_4184070734605111682_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=174925&_nc_eui2=AeGOSsxG_QiZtl-VBS042DvYPHl3ilpOpoA8eXeKWk6mgCfdHu-Pw1y2KJCpdadeDqvavoW5KE-1NVK49dOR4jcZ&_nc_ohc=-SnehzfpSjwAX9MmGqR&_nc_oc=AQkWpEJKmYf_Pdln790Sr9vPoqWQ5YKUUVg9unkHh7fhrykvOVWbsFgfhBc4iSHP8uo&_nc_ht=scontent.fdac7-1.fna&oh=00_AT9SX8bvRGSXDOxp3Iq02qCC0EGo9pGWUEGL-VdIApoOxQ&oe=62EC3E4C" alt="" />
                     </div>
-                    <div class="content">
+                    <div className="content">
                         <h2>Ariful Islam <br />
                             <span>VICE PRECIDENT</span></h2>
                     </div>
                 </div>
-                <div class="box">
-                    <div class="imgBox">
-                        <img src="https://image.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg" alt="" />
+                <div className="box">
+                    <div className="imgBox">
+                        <img src="./images/Shakil.jpg" alt="" />
                     </div>
-                    <div class="content">
+                    <div className="content">
                         <h2>Shakil Ahmed Khan<br />
                             <span>SECRETARY</span></h2>
                     </div>
                 </div>
-                <div class="box">
-                    <div class="imgBox">
+                <div className="box">
+                    <div className="imgBox">
                         <img src="https://scontent.fdac7-1.fna.fbcdn.net/v/t39.30808-6/279708463_2921770051447886_5439681024252892163_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeG32s05n4sWtOswauRbY0-QePtatH7TzU14-1q0ftPNTZyI4UUEZMn4Ey6N9nwmlHpjr7Q4qTS5lQcPUB-uWZ_V&_nc_ohc=faa0kMjkhjYAX8SQ1wG&_nc_oc=AQnfC1WSYHjhdqDS3ew03ZdKmygABoG3Lsgrv2kW9NC4eST_C8ydOzBntnvmj7noJRI&tn=1Eiy8ybYxLtgfG3l&_nc_ht=scontent.fdac7-1.fna&oh=00_AT_n7zfYfLGN9NVEkGrZ6Cq6Oo57GX17wdr75aZM9UY_4w&oe=62EAB45E" alt="" />
                     </div>
-                    <div class="content">
+                    <div className="content">
                         <h2>Nabid Hasan<br />
                             <span>PROJECT MANAGER</span></h2>
                     </div>
                 </div>
-                <div class="box">
-                    <div class="imgBox">
+                <div className="box">
+                    <div className="imgBox">
                         <img src="https://scontent.fdac7-1.fna.fbcdn.net/v/t39.30808-6/277802870_1605790496459065_4753792774501787359_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEbfmcWeOIAgWRhsVsB3QWsZuVZ5VOJo-Rm5VnlU4mj5BE1YAEUuu_EOm80zwMdFdK_dsuIMzipky4Op6fXlpft&_nc_ohc=r8lpVxCFbMkAX8cpDT6&_nc_ht=scontent.fdac7-1.fna&oh=00_AT9d7rzY_1893Yj-4gV7s61uE5Iz2SmqUsIwxx3RpJ4KNg&oe=62EB58D8" alt="" />
                     </div>
-                    <div class="content">
+                    <div className="content">
                         <h2>Fahad Bin Aziz<br />
                             <span>MEDIA MANAGER</span></h2>
                     </div>
                 </div>
-                <div class="box">
-                    <div class="imgBox">
+                <div className="box">
+                    <div className="imgBox">
                         <img src="https://scontent.fdac7-1.fna.fbcdn.net/v/t39.30808-6/273169042_320294090035764_4370449685666685955_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeELTTwFDlVcX_dh0F2qZZ9koDxQ_YQAhKWgPFD9hACEpc5aGntUUSHpLApqPGc3MqomVaIFirmbxI5M0QaxNQux&_nc_ohc=XmVyL5QJLuMAX__CorE&_nc_ht=scontent.fdac7-1.fna&oh=00_AT-FOPmaFCNcN17_dznF8U_luIGyws79K68F7kOT3yKPHw&oe=62EBE822" alt="" />
                     </div>
-                    <div class="content">
+                    <div className="content">
                         <h2>Rafiun Israt<br />
                             <span>PUBLIC SPOKEN PERSON</span></h2>
                     </div>
                 </div>
-                <div class="box">
-                    <div class="imgBox">
+                <div className="box">
+                    <div className="imgBox">
                         <img src="https://scontent.fdac7-1.fna.fbcdn.net/v/t39.30808-6/273710188_1058253164720920_1968900449200685059_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeEkpjvDcLWuu7jIM3X-yNCQy18N7j9nYhTLXw3uP2diFH9zfN7QBdnj1Q5EbnGpCAa4y3CNvD0jeqIw67Ucpnpu&_nc_ohc=Okz85uKzuKAAX8us46z&_nc_ht=scontent.fdac7-1.fna&oh=00_AT9pseLGaSMiO8r46ygAzg1nnOAZenFP_EL_9BxsMu9dJQ&oe=62EB311B" alt="" />
                     </div>
-                    <div class="content">
+                    <div className="content">
                         <h2>Tutul Hossain<br />
                             <span>TREASURER</span></h2>
                     </div>
--- src/Pages/Notice/Notice.js
+++ src/Pages/Notice/Notice.js
@@ -3,54 +3,54 @@ import React from 'react';
 const Notice = () => {
     return (
         <div>
-            <section class="flex flex-col justify-center antialiased  text-gray-600 p-4">
-                <div class="flex flex-wrap justify-center -mx-4">
-                    <div class="w-full px-4">
-                        <div class="text-center mx-auto mb-[60px] lg:mb-20 max-w-[510px]">
-                            <span class="font-semibold text-lg  mb-2 block text-white">
+            <section className="flex flex-col justify-center antialiased  text-gray-600 p-4">
+                <div className="flex flex-wrap justify-center -mx-4">
+                    <div className="w-full px-4">
+                        <div className="text-center mx-auto mb-[60px] lg:mb-20 max-w-[510px]">
+                            <span className="font-semibold text-lg  mb-2 block text-white">
                                 Our Notices
                             </span>
-                            <h2 class=" font-bold text-3xl sm:text-4xl md:text-[40px] text-white mb-4">
+                            <h2 className=" font-bold text-3xl sm:text-4xl md:text-[40px] text-white mb-4">
                                 Recent Notice
                             </h2>
-                            <p class="text-base text-white">
+                            <p className="text-base text-white">
                                 There are many variations of passages of Lorem Ipsum available
                                 but the majority have suffered alteration in some form.
                             </p>
                         </div>
                     </div>
                 </div>
-                <div class="h-full">
+                <div className="h-full">
                     {/* <!-- Card --> */}
-                    <div class="max-w-2xl mx-auto  shadow-lg rounded-lg" style={{ backgroundColor: "#193d52" }}>
-                        <div class="px-6 py-5">
-                            <div class="flex items-start">
+                    <div className="max-w-2xl mx-auto  shadow-lg rounded-lg" style={{ backgroundColor: "#193d52" }}>
+                        <div className="px-6 py-5">
+                            <div className="flex items-start">
 
                                 {/* <!-- Card content --> */}
-                                <div class="flex-grow truncate">
+                                <div className="flex-grow truncate">
                                     {/* <!-- Card header --> */}
-                                    <div class="w-full sm:flex justify-between items-center mb-3">
+                                    <div className="w-full sm:flex justify-between items-center mb-3">
                                         {/* <!-- Title --> */}
-                                        <h2 class="text-2xl leading-snug font-extrabold text-gray-50 truncate mb-1 sm:mb-0">Simple Design Tips</h2>
+                                        <h2 className="text-2xl leading-snug font-extrabold text-gray-50 truncate mb-1 sm:mb-0">Simple Design Tips</h2>
                                         {/* <!-- Notice issue date --> */}
-                                        <div class="flex-shrink-0 flex items-center space-x-3 sm:ml-2">
-                                            <div class="flex items-center text-left text-sm font-medium text-indigo-100 hover:text-white group focus:outline-none focus-visible:border-b focus-visible:border-indigo-100">
+                                        <div className="flex-shrink-0 flex items-center space-x-3 sm:ml-2">
+                                            <div className="flex items-center text-left text-sm font-medium text-indigo-100 hover:text-white group focus:outline-none focus-visible:border-b focus-visible:border-indigo-100">
 
-                                                <span>Mar 15 2022 <span class="sr-only">likes</span></span>
+                                                <span>Mar 15 2022 <span className="sr-only">likes</span></span>
                                             </div>
 
                                         </div>
                                     </div>
                                     {/* <!-- Card body --> */}
-                                    <div class="flex items-end justify-between whitespace-normal">
+                                    <div className="flex items-end justify-between whitespace-normal">
                                         {/* <!-- Paragraph --> */}
-                                        <div class="max-w-md text-indigo-100">
-                                            <p class="mb-2">Lorem ipsum dolor sit amet, consecte adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore.</p>
+                                        <div className="max-w-md text-indigo-100">
+                                            <p className="mb-2">Lorem ipsum dolor sit amet, consecte adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore.</p>
                                         </div>
                                         {/* <!-- More link --> */}
-                                        <a class="flex-shrink-0 flex items-center justify-center text-indigo-600 h-10 " href="#0">
-                                            {/* <span class="block font-bold"><span class="sr-only">Read more</span>X</span> */}
-                                            <span class="block font-bold text-white text-xs">Read more...</span>
+                                        <a className="flex-shrink-0 flex items-center justify-center text-indigo-600 h-10 " href="#0">
+                                            {/* <span className="block font-bold"><span className="sr-only">Read more</span>X</span> */}
+                                            <span className="block font-bold text-white text-xs">Read more...</span>
                                         </a>
                                     </div>
                                 </div>
@@ -59,38 +59,38 @@ const Notice = () => {
                     </div>
                 </div>
             </section>
-            <section class="flex flex-col justify-center antialiased  text-gray-600 p-4">
-                <div class="h-full">
+            <section className="flex flex-col justify-center antialiased  text-gray-600 p-4">
+                <div className="h-full">
                     {/* <!-- Card --> */}
-                    <div class="max-w-2xl mx-auto bg-indigo-600 shadow-lg rounded-lg">
-                        <div class="px-6 py-5">
-                            <div class="flex items-start">
+                    <div className="max-w-2xl mx-auto bg-indigo-600 shadow-lg rounded-lg">
+                        <div className="px-6 py-5">
+                            <div className="flex items-start">
 
                                 {/* <!-- Card content --> */}
-                                <div class="flex-grow truncate">
+                                <div className="flex-grow truncate">
                                     {/* <!-- Card header --> */}
-                                    <div class="w-full sm:flex justify-between items-center mb-3">
+                                    <div className="w-full sm:flex justify-between items-center mb-3">
                                         {/* <!-- Title --> */}
-                                        <h2 class="text-2xl leading-snug font-extrabold text-gray-50 truncate mb-1 sm:mb-0">Simple Design Tips</h2>
+                                        <h2 className="text-2xl leading-snug font-extrabold text-gray-50 truncate mb-1 sm:mb-0">Simple Design Tips</h2>
                                         {/* <!-- Notice issue date --> */}
-                                        <div class="flex-shrink-0 flex items-center space-x-3 sm:ml-2">
-                                            <div class="flex items-center text-left text-sm font-medium text-indigo-100 hover:text-white group focus:outline-none focus-visible:border-b focus-visible:border-indigo-100">
+                                        <div className="flex-shrink-0 flex items-center space-x-3 sm:ml-2">
+                                            <div className="flex items-center text-left text-sm font-medium text-indigo-100 hover:text-white group focus:outline-none focus-visible:border-b focus-visible:border-indigo-100">
 
-                                                <span>Mar 15 2022 <span class="sr-only">likes</span></span>
+                                                <span>Mar 15 2022 <span className="sr-only">likes</span></span>
                                             </div>
 
                                         </div>
                                     </div>
                                     {/* <!-- Card body --> */}
-                                    <div class="flex items-end justify-between whitespace-normal">
+                                    <div className="flex items-end justify-between whitespace-normal">
                                         {/* <!-- Paragraph --> */}
-                                        <div class="max-w-md text-indigo-100">
-                                            <p class="mb-2">Lorem ipsum dolor sit amet, consecte adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore.</p>
+                                        <div className="max-w-md text-indigo-100">
+                                            <p className="mb-2">Lorem ipsum dolor sit amet, consecte adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore.</p>
                                         </div>
                                         {/* <!-- More link --> */}
-                                        <a class="flex-shrink-0 flex items-center justify-center text-indigo-600 h-10 " href="#0">
-                                            {/* <span class="block font-bold"><span class="sr-only">Read more</span>X</span> */}
-                                            <span class="block font-bold text-white text-xs">Read more...</span>
+                                        <a className="flex-shrink-0 flex items-center justify-center text-indigo-600 h-10 " href="#0">
+                                            {/* <span className="block font-bold"><span className="sr-only">Read more</span>X</span> */}
+                                            <span className="block font-bold text-white text-xs">Read more...</span>
                                         </a>
                                     </div>
                                 </div>
