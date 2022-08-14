3d0408a24f77da9b629e4d7bd488b87cf14c825d
Tanjir Ahmed <74643721+AhmdShuvo@users.noreply.github.com>
2022-08-14 08:58:31 +0600

    photoGallary

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,12 +3,15 @@ import Footer from '../Shared/Footer/Footer';
 import Navigation from '../Shared/Navbar/Navigation';
 import Hero from '../Home/Hero'
 import CommitteeMemmbers from './CommitteeMemmbers';
+import Gallary from '../PhotoGallary]/Gallary';
+
 
 const Home = () => {
     return (
     <>
     <Hero></Hero>
     <CommitteeMemmbers></CommitteeMemmbers>
+    <Gallary></Gallary>
    
     
     </>
