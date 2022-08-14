ac18d516b0c19b44673882a7044185c3ac3528a4
Tanjir Ahmed <74643721+AhmdShuvo@users.noreply.github.com>
2022-08-14 21:15:52 +0600

    remove irralivent content

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import Footer from '../Shared/Footer/Footer';
-import Navigation from '../Shared/Navbar/Navigation';
 import Hero from '../Home/Hero'
 import CommitteeMemmbers from './CommitteeMemmbers';
 
