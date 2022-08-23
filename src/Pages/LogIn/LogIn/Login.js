fc3ed3adb690376e705af8fd899c130784306deb
Tanjir Ahmed <74643721+AhmdShuvo@users.noreply.github.com>
2022-08-23 21:58:38 +0600

    a

--- src/Pages/LogIn/LogIn/Login.js
+++ src/Pages/LogIn/LogIn/Login.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Login = () => {
     return (
         <div>
-            <h2>hhhhh</h2>
+            <h2>92387289runcwajhdjkcfhascknmxfsjfskhcfnaxksfhmsdjxf,kzmkfs</h2>
         </div>
     );
 };
